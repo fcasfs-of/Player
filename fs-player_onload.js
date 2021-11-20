f477391b0f3380fee1cec39baca6f4d63b109dc5
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-11-20 20:06:20 -0300

    Update fs-player_onload.js

--- fs-player_onload.js
+++ fs-player_onload.js
@@ -2,16 +2,18 @@
 function fcasmdplex_load_url() { return fcasmdplex_loadex.openex.url(); } function fcasmdplex_load_server(){ return fcasmdplex_loadex.openex.server(); }    
 
 
-function download_video_set(d, v) {  var ex_loadex_downnd=fmdplex_loadex_txv(v.getAttribute("src"));  if(ex_loadex_downnd==""){   }  else {  
- z_setid_attr(d,'href', fmdplex_loadex_txv(v.getAttribute("src")));             z_setid_attr(d,'download', fmdplex_loadex_txv(v.getAttribute("src")));    v.pause();  
-                                                                                                                                          }   }  
-
  lightbox_inner_addcustom("");
 
 
 function box_in_set(az,id,idd,t,fl,cp,po, ds,dssa){   
  lightbox_inner(az, id, { type: t, lightbox:'true', caption: cp, mute:'false', controls:'true', autoplay:'false', loop:'true', volume:ds, currentTime:dssa, playbackRate:'1', poster: po }, { caption:{ enable:true }, thumbs:{ enable:false }}, { thumbs:{ id:'' }, caption:{ id: fl }});  
 }
+
+function download_video_set(d, v) {  var ex_loadex_downnd=fmdplex_loadex_txv(v.getAttribute("src"));  if(ex_loadex_downnd==""){   }  else {  
+ z_setid_attr(d,'href', fmdplex_loadex_txv(v.getAttribute("src")));             z_setid_attr(d,'download', fmdplex_loadex_txv(v.getAttribute("src")));    
+ v.pause();  
+box_in_set("false","light_box_modal","box_btns","video",ex_loadex_downnd,"video",fmdplex_loadex_txv(v.getAttribute("poster")), v.volume, v.currentTime);
+}   }  
 
 
 

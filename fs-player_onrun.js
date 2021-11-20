b1744008c1c80820d715a1b13e0b0882ca25cc89
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-11-20 20:17:59 -0300

    Update fs-player_onrun.js

--- fs-player_onrun.js
+++ fs-player_onrun.js
@@ -5,13 +5,13 @@ var link_flvid="";
 function playelisvid_setmode_file(zplaylist_tbl){
 set_player_IconsColor("#1874CD");   
 set_playlist_videoplayer("zpreviewdemovideinz", zplaylist_tbl,"");
-set_player_settings().size("zpreviewdemovideinz", "100%");   set_player_settings().position.center("zpreviewdemovideinz");    /*set_icon_controls("");*/    var app_plconfig_playlist={ search:true, autoplay:false };  var app_plconfig = { Playlist:app_plconfig_playlist, Controls:true, Title:true };  var controls_settings_buttons = { Icon:false, Speed_Control:true, Stop:true, Download:false };  set_player_controls_settings(controls_settings_buttons);set_player_config(app_plconfig);        /*set_player_settings().version_update();*/   set_player_settings().Button_Light(false);    /*set_player_settings().Scroll_to_MiniPlayer("zpreviewdemovideinz");*/
+set_player_settings().size("zpreviewdemovideinz", "100%");   set_player_settings().position.center("zpreviewdemovideinz");    /*set_icon_controls("");*/    var app_plconfig_playlist={ search:true, autoplay:false };  var app_plconfig = { Playlist:app_plconfig_playlist, Controls:true, Title:true };  var controls_settings_buttons = { Icon:false, Speed_Control:true, Stop:true, Download:true };  set_player_controls_settings(controls_settings_buttons);set_player_config(app_plconfig);        set_player_settings().version_update();   set_player_settings().Button_Light(false);    /*set_player_settings().Scroll_to_MiniPlayer("zpreviewdemovideinz");*/
 }
 
 function playevid_setmode_file(f, t){
 set_player_IconsColor("#1874CD");   
 set_file_videoplayer("zpreviewdemovideinz", f, t, "","", "", "", "true");    
-set_player_settings().size("zpreviewdemovideinz", "100%");   set_player_settings().position.center("zpreviewdemovideinz");    /*set_icon_controls("");*/    var app_plconfig_playlist={ search:true, autoplay:false };  var app_plconfig = { Playlist:app_plconfig_playlist, Controls:true, Title:true };  var controls_settings_buttons = { Icon:false, Speed_Control:true, Stop:true, Download:false };  set_player_controls_settings(controls_settings_buttons);set_player_config(app_plconfig);        /*set_player_settings().version_update();*/   set_player_settings().Button_Light(false);    /*set_player_settings().Scroll_to_MiniPlayer("zpreviewdemovideinz");*/
+set_player_settings().size("zpreviewdemovideinz", "100%");   set_player_settings().position.center("zpreviewdemovideinz");    /*set_icon_controls("");*/    var app_plconfig_playlist={ search:true, autoplay:false };  var app_plconfig = { Playlist:app_plconfig_playlist, Controls:true, Title:true };  var controls_settings_buttons = { Icon:false, Speed_Control:true, Stop:true, Download:true };  set_player_controls_settings(controls_settings_buttons);set_player_config(app_plconfig);        set_player_settings().version_update();   set_player_settings().Button_Light(false);    /*set_player_settings().Scroll_to_MiniPlayer("zpreviewdemovideinz");*/
 }
 
 var mdpl_handleFileSelectsa=[];

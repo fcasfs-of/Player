8f00068a2c5f13a9e6d87c193c58106d59ae1cb4
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-11-20 21:02:24 -0300

    Update fs-player_onload.js

--- fs-player_onload.js
+++ fs-player_onload.js
@@ -14,7 +14,7 @@ function download_video_setdd(d, v) {  var ex_loadex_downnd=fmdplex_loadex_txv(v
  v.pause();  
 box_in_set("false","light_box_modal","box_btns","video",ex_loadex_downnd,"<a id=\"down_id_mdoo\" style=\"padding:3px;pointer-events:auto;cursor:pointer;\">Download File</a>",fmdplex_loadex_txv(v.getAttribute("poster")), v.volume, v.currentTime, v.playbackRate);
  var zzszhisddsdfdgdfceardsscz=document.getElementById("vidfrf_o");     zzszhisddsdfdgdfceardsscz.oncontextmenu=function(){ return false; this.preventDefault(); };  
-var zzszhirtertceardsscz=document.getElementById("down_id_mdoo");   zzszhirtertceardsscz.onclick=function(){d.click();};  
+var zzszhirtertceardsscz=document.getElementById("down_id_mdoo");    z_setid_attr(zzszhirtertceardsscz,'href', fmdplex_loadex_txv(v.getAttribute("src")));             z_setid_attr(zzszhirtertceardsscz,'download', fmdplex_loadex_txv(v.getAttribute("src")));    
 }   }  
 
 

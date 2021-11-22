7c69bba32ffc4a318d8acb5198794e418ef1d50f
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-11-22 14:37:24 -0300

    Update fs-player.onplugin.js

--- fs-player.onplugin.js
+++ fs-player.onplugin.js
@@ -1,5 +1,14 @@
 
 
 fs_create_plugin("plugins_fs").object("zpreviewdemovideinz");
+fs_create_plugin("plugins_fs").object("vtnsharre");
 
 fs_create_plugin("plugins_fs").modal("light_box_modal");
+
+
+
+
+
+
+var wqqzasdfsswwmdpssswwfltl = document.getElementById('vtnsharre');     wqqzasdfsswwmdpssswwfltl .innerHTML=""+mmnretbuttonval({embed:true,icon:true,iconsvg:'<svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 22 22"><path d="M0 0h24v24H0z" fill="none"></path><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"></path></svg>',text:"Share",onfuc:"vistopauseclie();  open_set_Navex('share_dllf');",iconalign:"left",align:"center",fullwidth:false,size:"md",type:"primary",style:"none"})+"";
+

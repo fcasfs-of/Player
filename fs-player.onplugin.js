04167437dc71c07cf46c5c2f1359bc8bfe6e9ab8
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-11-22 14:57:49 -0300

    Update fs-player.onplugin.js

--- fs-player.onplugin.js
+++ fs-player.onplugin.js
@@ -10,9 +10,6 @@ fs_create_plugin("plugins_fs").modal("light_box_modal");
 
 
 
-var form_linkinssharelpd= "Share/Page";
-var cusrr_sharessddd='<iframe id="Share" allowfullscreen="" src="'+form_linkinssharelpd+'" style=" " width="100%" height="360px" frameborder="0"></iframe>';
-
-var wqqzasdfsswwmdpssswwfltl = document.getElementById('vtnsharre');     wqqzasdfsswwmdpssswwfltl.innerHTML=""+cusrr_sharessddd+"";
+var wqqzasdfsswwmdpssswwfltl = document.getElementById('vtnsharre');     wqqzasdfsswwmdpssswwfltl.innerHTML=""+"";
 
 

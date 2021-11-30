c84fcbbe605b265cea8e2338ae1e781f268478c2
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-11-30 15:22:45 -0300

    Update index.js

--- Server/Files/index.js
+++ Server/Files/index.js
@@ -1,3 +1,3 @@
-var server_fs_itens={};
+var server_fs_itens=[];
 
-server_fs_itens[712690675063432847324893]={"file":"https://archive.org/download/tokusastsu-hit-song-collection/Sekai%20Ninja%20Sen%20Jiraiya%20-%20Full%20Songs.ia.mp4","poster":"https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Sekai%20Ninja%20Sen%20Jiraiya%20-%20Full%20Songs_000174.jpg","title":"Sekai Ninja Sen Jiraiya - Full Songs"};
+var server_fs_itens[712690675063432847324893]={"file":"https://archive.org/download/tokusastsu-hit-song-collection/Sekai%20Ninja%20Sen%20Jiraiya%20-%20Full%20Songs.ia.mp4","poster":"https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Sekai%20Ninja%20Sen%20Jiraiya%20-%20Full%20Songs_000174.jpg","title":"Sekai Ninja Sen Jiraiya - Full Songs"};

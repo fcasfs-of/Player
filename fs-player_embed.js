257b8c7db1436ca9e8c289ab26e47d9eeb52211f
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-05 13:20:53 -0300

    Update fs-player_embed.js

--- fs-player_embed.js
+++ fs-player_embed.js
@@ -1,6 +1,6 @@
-function fs_embed_callb(vd, fd)
+function fs_embed_callb(vd, fd){
 var zssfdggfsasd_iswe = ""+fcasmdplex_loadex.open("?embed=")+"";
-if (zssfdggfsasd_iswe=="true"){  vd();  }  else{ fd(); }
+if (zssfdggfsasd_iswe=="true"){  vd();  }  else{  fd();  }
 }
 
 

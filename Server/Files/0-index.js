2984fe706f1593e81c9ed18bc0e0d67998505aaa
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-06 18:46:45 -0300

    Update 0-index.js

--- Server/Files/0-index.js
+++ Server/Files/0-index.js
@@ -1,10 +1,10 @@
 var loacol9d="https://fcasfs-of.github.io/Player/Server/Files";
 var targf_ddlist_filesaaaet = '';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/711600070003432847011811.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/[MV] Ryukendo_000354.jpg" class="fa"/>[MV] Ryukendo</a></li>';
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060111840304810.html"+'">Jiban the movie - All Songs</a></li>';
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060132807324890.html"+'">Kidou Keiji Jiban - Hit Song Collection</a></li>';
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060132805020894.html"+'">Kamen Rider BLACK RX - Full Songs</a></li>';
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/712690675063432847324893.html"+'">Sekai Ninja Sen Jiraiya - Full Songs</a></li>';
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060111840304810.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Movie - All Songs_000508.jpg" class="fa"/>Jiban the movie - All Songs</a></li>';
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060132807324890.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Kidou Keiji Jiban - Hit Song Collection_000027.jpg" class="fa"/>Kidou Keiji Jiban - Hit Song Collection</a></li>';
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060132805020894.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Kamen Rider BLACK RX - Full Songs_002036.jpg" class="fa"/>Kamen Rider BLACK RX - Full Songs</a></li>';
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/712690675063432847324893.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/_00.jpg" class="fa"/>Sekai Ninja Sen Jiraiya - Full Songs</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'';
 
 

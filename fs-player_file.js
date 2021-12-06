b321d4d4096296e04fa819521ab212bcd7a12416
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-06 19:36:29 -0300

    Update fs-player_file.js

--- fs-player_file.js
+++ fs-player_file.js
@@ -2,10 +2,20 @@
 Reproduce_aLink(zsssasd_iswe,"zpreviewdemovideinz");
 
 
+
+  var fdsforfgdgm = document.querySelector('.comments');
+  fdsforfgdgm.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    box_in_set("false","lightcomme_box_modal","","inner",wqqzasdfsfgsswwfltl,"","", 0, 0, 1);
+
   var forfgdgm = document.querySelector('.comments__form');
   forfgdgm.addEventListener('submit', function(e) {
     e.preventDefault();
   });
+
+  });
+
 
 
 

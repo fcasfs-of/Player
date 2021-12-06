555de70b10e4db88b1f83072b569c27e06814b20
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-06 20:29:20 -0300

    Update fs-player_file.js

--- fs-player_file.js
+++ fs-player_file.js
@@ -3,9 +3,3 @@ Reproduce_aLink(zsssasd_iswe,"zpreviewdemovideinz");
 
 
 
-var forfgdgm = document.querySelectorAll('.comments__form');
-  forfgdgm.onsubmit = function(e) {
-    e.preventDefault();
-  });
-
-

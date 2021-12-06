e374de08e2a1c3629e48bf905b4bc4bf57217dd2
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-06 19:28:53 -0300

    Update fs-player.onplugin.js

--- fs-player.onplugin.js
+++ fs-player.onplugin.js
@@ -6,10 +6,15 @@ fs_create_plugin("plugins_fs").object("vtnsharre");
 fs_create_plugin("plugins_fs").modal("light_box_modal");
 
 
+fs_create_plugin("plugins_fs").modal("lightcomme_box_modal")
+
 
 
 
 
 var wqqzasdfsswwmdpssswwfltl = document.getElementById('vtnsharre');     wqqzasdfsswwmdpssswwfltl.innerHTML=""+"";
 
+
+var wqqzasdfsfgsswwfltl = document.getElementById('lightcomme_box_modal');   
+wqqzasdfsfgsswwfltl.innerHTML=""+' <div class="comments">    <h2>Leave a comment</h2>    <form class="comments__form">      <div class="comments__form-info">        <div class="comments__form-field">          <input id="comments__form-label-name" name="author" placeholder="Name" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-name">            <span class="comments__form-label-text">Name</span>          </label>        </div>        <div class="comments__form-field">          <input id="comments__form-label-mail" name="author" placeholder="E-mail" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-mail">            <span class="comments__form-label-text">E-mail</span>          </label>        </div>        <div class="comments__form-field">          <input id="comments__form-label-site" name="author" placeholder="Website" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-site">            <span class="comments__form-label-text">Website</span>          </label>        </div>      </div>      <div class="comments__form-text">    <div class="comments__form-field">          <textarea id="comments__form-label-text" name="author" placeholder="Express your thoughts" type="text" class="comments__form-input comments__form-textarea"></textarea>          <label class="comments__form-label" for="comments__form-label-text">            <span class="comments__form-label-text">Express your thoughts</span>          </label>        </div>      </div>  <input name="submit" type="submit" id="submit" class="comments__form-submit" value="Post Comment">    </form>    <div class="comments__list">      <!-- Comments -->     </div>  </div>'+"";
 

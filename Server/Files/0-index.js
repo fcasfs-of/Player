d3a775c4e780d816fbbb2429e29328457cc8e944
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-06 17:32:24 -0300

    Update 0-index.js

--- Server/Files/0-index.js
+++ Server/Files/0-index.js
@@ -1,5 +1,6 @@
 var loacol9d="https://fcasfs-of.github.io/Player/Server/Files";
 var targf_ddlist_filesaaaet = '';
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;"  href="'+loacol9d+"/714590675060111840304810.html"+'">Jiban the movie - All Songs</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;"  href="'+loacol9d+"/714590675060132807324890.html"+'">Kidou Keiji Jiban - Hit Song Collection</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;"  href="'+loacol9d+"/714590675060132805020894.html"+'">Kamen Rider BLACK RX - Full Songs</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;"  href="'+loacol9d+"/712690675063432847324893.html"+'">Sekai Ninja Sen Jiraiya - Full Songs</a></li>';

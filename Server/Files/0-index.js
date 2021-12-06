a29d1504fda0e6a2b13311d2ce83eea63f4f0b56
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-06 18:16:07 -0300

    Update 0-index.js

--- Server/Files/0-index.js
+++ Server/Files/0-index.js
@@ -1,6 +1,6 @@
 var loacol9d="https://fcasfs-of.github.io/Player/Server/Files";
 var targf_ddlist_filesaaaet = '';
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/[MV] Ryukendo_000294.jpg" class="fa"/><a style="display:none;" href="'+loacol9d+"/711600070003432847011811.html"+'">[MV] Ryukendo</a></li>';
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/711600070003432847011811.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/[MV] Ryukendo_000294.jpg" class="fa"/>[MV] Ryukendo</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060111840304810.html"+'">Jiban the movie - All Songs</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060132807324890.html"+'">Kidou Keiji Jiban - Hit Song Collection</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060132805020894.html"+'">Kamen Rider BLACK RX - Full Songs</a></li>';

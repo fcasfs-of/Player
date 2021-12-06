5e36d79219e029971a1e90cfb7f593b978836038
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-06 14:00:40 -0300

    Update 0-index.js

--- Server/Files/0-index.js
+++ Server/Files/0-index.js
@@ -1,7 +1,6 @@
 var loacol9d="https://fcasfs-of.github.io/Player/Server/Files";
-var server_fs_itens=[
-    {"name":"Kidou Keiji Jiban - Hit Song Collection", "fl":loacol9d+"/714590675060132807324890.html"},
-    {"name":"Kamen Rider BLACK RX - Full Songs", "fl":loacol9d+"/714590675060132805020894.html"},
-    {"name":"Sekai Ninja Sen Jiraiya - Full Songs", "fl":loacol9d+"/712690675063432847324893.html"},
-    
+var server_fs_itens = [
+    {name:"Kidou Keiji Jiban - Hit Song Collection",fl:loacol9d+"/714590675060132807324890.html"},
+    {name:"Kamen Rider BLACK RX - Full Songs",fl:loacol9d+"/714590675060132805020894.html"},
+    {name:"Sekai Ninja Sen Jiraiya - Full Songs",fl:loacol9d+"/712690675063432847324893.html"},
 ];

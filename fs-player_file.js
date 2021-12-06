2f9331e674acf0673cf3ebb564879bf5bcc02541
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-06 20:24:00 -0300

    Update fs-player_file.js

--- fs-player_file.js
+++ fs-player_file.js
@@ -3,10 +3,3 @@ Reproduce_aLink(zsssasd_iswe,"zpreviewdemovideinz");
 
 
 
-var wqqzasdfdffsswwmdpssswwfltl = document.getElementById('lightcomme_box_modal');     wqqzasdfdffsswwmdpssswwfltl.innerHTML="<br/><br/>"+wqqzasdfsfgsswwfltl+"<br/><br\>";
-var forfgdgm = document.querySelectorAll('.comments__form');
-  forfgdgm.onsubmit = function(e) {
-    e.preventDefault();
-  });
-
-

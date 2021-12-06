7b10b28fe4519740bc08f611bbe7cb74481df338
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-06 12:32:01 -0300

    Update and rename index.js to 0-index.js

---
+++ Server/Files/0-index.js
@@ -0,0 +1,7 @@
+var loacol9d="https://fcasfs-of.github.io/Player/Server/Files";
+var server_fs_itens=[
+    {"Kidou Keiji Jiban - Hit Song Collection",loacol9d+"/714590675060132807324890.html"},
+    {"Kamen Rider BLACK RX - Full Songs",loacol9d+"/714590675060132805020894.html"},
+    {"Sekai Ninja Sen Jiraiya - Full Songs",loacol9d+"/712690675063432847324893.html"},
+    
+];

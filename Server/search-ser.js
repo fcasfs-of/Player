b352e91522193c0cab6a174611dd4cbb95bb7cb8
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-07 18:48:18 -0300

    Update search-ser.js

--- Server/search-ser.js
+++ Server/search-ser.js
@@ -1,4 +1,6 @@
 
+var loacol_thum = "ztrue";
+
 var targf_list_filesaaaet = document.getElementById("f_list_filesaaa");  targf_list_filesaaaet.innerHTML=""+targf_ddlist_filesaaaet;
 var targf_list_fidsflesaaaet = document.getElementById("q");  targf_list_fidsflesaaaet.value=fmdplex_loadex_txv(decodeURIComponent(fcasmdplex_loadex.open("?q=").replace(/\+/g,  " ")));    mspl_search_list();
 var tadrgf_list_fidsflesaaaet = document.getElementById("f_mfdval_Input");   document.title="FS Player - Online: Search: "+targf_list_fidsflesaaaet.value;

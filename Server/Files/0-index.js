71aa955c01295ea2c510c4a0e929d8e022ec8d94
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-07 16:45:33 -0300

    Update 0-index.js

--- Server/Files/0-index.js
+++ Server/Files/0-index.js
@@ -10,6 +10,7 @@ targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" hr
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060132805020894.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Kamen Rider BLACK RX - Full Songs_002036.jpg" class="fa"/>'+truncgate("Kamen Rider BLACK RX - Full Songs")+'</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/712690675063432847324893.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Sekai Ninja Sen Jiraiya - Full Songs_000294.jpg" class="fa"/>'+truncgate("Sekai Ninja Sen Jiraiya - Full Songs")+'</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/711678979903432040410011.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Space_Sheriff_Memorial - PV_000267.jpg" class="fa"/>'+truncgate("Space_Sheriff_Memorial - PV")+'</a></li>';
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/411400070043002847020800.html"+'"><img src="https://archive.org/download/tokusatsu-compilation-musics-all/tokusatsu-compilation-musics-all.thumbs/Tokusatsu - Compilation Musics_000164.jpg" class="fa"/>'+truncgate("Tokusatsu - Compilation Musics")+'</a></li>';
 
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'';
 

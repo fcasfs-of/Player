12b0fb38f4a7fdb575f9ddb3784104c6e7d43006
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-07 19:56:14 -0300

    Update search-se.js

--- Server/search-se.js
+++ Server/search-se.js
@@ -1,5 +1,5 @@
 
-   var loacol_thum = "false";   
+   var loacol_thum = "false";     
 
 function truncateWithEllipseds(text, max) {    return text.substr(0,max-1)+(text.length>max?'&hellip;':''); }
 function truncate(input) {   return truncateWithEllipseds(input, 45);};

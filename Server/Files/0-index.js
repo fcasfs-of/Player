093cb5a6ebe0c599492cd1c0bc2e79d07314bc18
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-06 21:42:26 -0300

    Update 0-index.js

--- Server/Files/0-index.js
+++ Server/Files/0-index.js
@@ -5,6 +5,8 @@ targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" hr
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060132807324890.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Kidou Keiji Jiban - Hit Song Collection_000027.jpg" class="fa"/>Kidou Keiji Jiban - Hit Song Collection</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060132805020894.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Kamen Rider BLACK RX - Full Songs_002036.jpg" class="fa"/>Kamen Rider BLACK RX - Full Songs</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/712690675063432847324893.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Sekai Ninja Sen Jiraiya - Full Songs_000294.jpg" class="fa"/>Sekai Ninja Sen Jiraiya - Full Songs</a></li>';
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/711678979903432040410011.html"+'"><img src="https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Space_Sheriff_Memorial - PV_000267.jpg" class="fa"/>Space_Sheriff_Memorial - PV</a></li>';
+
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'';
 
 

3139dc869d0ff60ac7544c44fb96525e746b39dd
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-07 20:43:55 -0300

    Update fs-player.onplugin.js

--- fs-player.onplugin.js
+++ fs-player.onplugin.js
@@ -16,23 +16,6 @@ fs_create_plugin("emb-fs").object("lightembe_box_modal");
 
 
 
-function SelectTdfext(element) {
-    var doc = document, text = doc.getElementById(element), range, selection;    
-    if (doc.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(text);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();        
-        range = document.createRange();
-        range.selectNodeContents(text);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-}
-
-
-
 
 var wqqzasdfsswwmdpssswwfltl = document.getElementById('vtnsharre');     wqqzasdfsswwmdpssswwfltl.innerHTML=""+"";
 

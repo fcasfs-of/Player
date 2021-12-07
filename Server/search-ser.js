30f354e0cf427ca4bd0a5ad808f76db140aea96e
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-07 18:11:12 -0300

    Update search-ser.js

--- Server/search-ser.js
+++ Server/search-ser.js
@@ -1,6 +1,6 @@
 
 var targf_list_filesaaaet = document.getElementById("f_list_filesaaa");  targf_list_filesaaaet.innerHTML=""+targf_ddlist_filesaaaet;
 var targf_list_fidsflesaaaet = document.getElementById("q");  targf_list_fidsflesaaaet.value=fmdplex_loadex_txv(decodeURIComponent(fcasmdplex_loadex.open("?q=").replace(/\+/g,  " ")));    mspl_search_list();
-var tadrgf_list_fidsflesaaaet = document.getElementById("f_mfdval_Input");   documet.title="FS Player - Online: Search: "+targf_list_fidsflesaaaet.value;
+var tadrgf_list_fidsflesaaaet = document.getElementById("f_mfdval_Input");   document.title="FS Player - Online: Search: "+targf_list_fidsflesaaaet.value;
    //tadrgf_list_fidsflesaaaet.onsubmit=function(){location.href="https://fcasfs-of.github.io/Player/Server/search.htm?q="+targf_list_fidsflesaaaet.value;};
 

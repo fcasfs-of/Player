1ab4b5b02c0d8046051045a388a6b7e1f0a9f9ad
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-07 10:13:38 -0300

    Update fs-player.onplugin.js

--- fs-player.onplugin.js
+++ fs-player.onplugin.js
@@ -7,6 +7,7 @@ fs_create_plugin("plugins_fs").modal("light_box_modal");
 
 
 fs_create_plugin("emb-fs").object("lightcomme_box_modal");
+fs_create_plugin("emb-fs").object("lightembe_box_modal");
 
 
 
@@ -23,4 +24,9 @@ function setcoome(e){
   }
 
 var wqqzasdfsfgsswwfltl = ""+'<div class="comments">    <h2>Leave a comment</h2>    <form class="comments__form" onsubmit="setcoome(event)">   <span class="comments__form-submit" style="position:absolute;top:1px;right:8px" onclick="mdpl_ztoogle_dispNav(wqqzasdffgdgsswwmdpssswwfltl);">X</span>    <div class="comments__form-info">        <div class="comments__form-field">          <input id="comments__form-label-name" name="author" placeholder="Name" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-name">            <span class="comments__form-label-text">Name</span>          </label>        </div>        <div class="comments__form-field">          <input id="comments__form-label-mail" name="author" placeholder="E-mail" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-mail">            <span class="comments__form-label-text">E-mail</span>          </label>        </div>        <div class="comments__form-field">          <input id="comments__form-label-site" name="author" placeholder="Website" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-site">            <span class="comments__form-label-text">Website</span>          </label>        </div>      </div>      <div class="comments__form-text">    <div class="comments__form-field">          <textarea id="comments__form-label-text" name="author" placeholder="Express your thoughts" type="text" class="comments__form-input comments__form-textarea"></textarea>          <label class="comments__form-label" for="comments__form-label-text">            <span class="comments__form-label-text">Express your thoughts</span>          </label>        </div>      </div>  <input name="submit" type="submit" id="submit" class="comments__form-submit" value="Post Comment">    </form>  <br/>   <div class="comments__list">      <!-- Comments -->     </div>  </div><br/>'+"";
+
+
+var wqqzasdfsfgsswwfltlz = '<pre class="code wp-block-code code-html"><label>embed</label><code>&lt;iframe src="'+location.href+'" width="320" frameborder="0" height="240" style="height:100%;width:100%;" allowfullscreen=""&gt;&lt;/iframe&gt;</code><hr/><pre class="first wp-block-code"><code style="color:#fff;">?embed=true  -(preview of file)<br/>?info=true  -(view info file)</code></pre></pre>';
+ 
+var wqqzasdffgdgslightembe_box_modalpssswwfltl = document.getElementById('lightembe_box_modal');   wqqzasdffgdgslightembe_box_modalpssswwfltl.style.display="none";   wqqzasdffgdgslightembe_box_modalpssswwfltl.innerHTML=wqqzasdfsfgsswwfltlz;
 

2fe43820c8f3fe433e4b9a26bc70f9c72139b6c9
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-07 19:23:27 -0300

    Update fscloud-index.js

--- Server/Files/fscloud-index.js
+++ Server/Files/fscloud-index.js
@@ -8,8 +8,12 @@ function si_thud(inputz) {       var zsi_thudsss="";//<img class=\"fa\" style='d
   if(loacol_thum=="true"){ zsi_thudsss=inputz; }     return zsi_thudsss;   }    
 
 
+function add_fileset(d,m,bb,nn) {     d=d+'<li><a style="display:none;" href="'+m+'">'+si_thud("<img src=\""+bb+" class=\"fa\"/>")+truncgate(nn)+'</a></li>';    }    
+
+
 var loacol9d="https://fcasfs-of.github.io/Player/Server/Files/cloud";
 var targf_ddlist_filesaaaet = '';
+
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/711600070003432847011811.html"+'">'+si_thud("<img src=\"https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/[MV] Ryukendo_000354.jpg\" class=\"fa\"/>")+truncgate("[MV] Ryukendo")+'</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060111840304810.html"+'">'+si_thud("<img src=\"https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Movie - All Songs_000508.jpg\" class=\"fa\"/>")+truncgate("Jiban the movie - All Songs")+'</a></li>';
 targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+'<li><a style="display:none;" href="'+loacol9d+"/714590675060132807324890.html"+'">'+si_thud("<img src=\"https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Kidou Keiji Jiban - Hit Song Collection_000027.jpg\" class=\"fa\"/>")+truncgate("Kidou Keiji Jiban - Hit Song Collection")+'</a></li>';

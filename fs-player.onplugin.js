6db57f0e53f89d04dd2fdece51d954e9d9c9253b
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-07 11:16:55 -0300

    Update fs-player.onplugin.js

--- fs-player.onplugin.js
+++ fs-player.onplugin.js
@@ -33,7 +33,7 @@ var wqqzasdfsfgsswwfltlz = '<div><pre class="code wp-block-code code-html" oncli
  
 function vop_emjjg(){
 var wqqzasdffgdgsembeddlsswwfltl = document.getElementById('embe-ilss');
- copy.copy_text(decodeURIComponent(wqqzasdffgdgsembeddlsswwfltl.innerHTML));};
+ copy.copy_text('<iframe src="'+location.href+'" width="320" frameborder="0" height="240" style="height:100%;width:100%;" allowfullscreen=""></iframe>');};
 
 var wqqzasdffgdgsembeddlsswwfltl = document.getElementById('embe-ilss');
 var wqqzasdffgdgslightembe_box_modalpssswwfltl = document.getElementById('lightembe_box_modal');   wqqzasdffgdgslightembe_box_modalpssswwfltl.style.display="none";   wqqzasdffgdgslightembe_box_modalpssswwfltl.innerHTML=wqqzasdfsfgsswwfltlz;

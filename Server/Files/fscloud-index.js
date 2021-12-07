5b08ef23a9bc917a3e546ccc8dcbd69bcf6f2b77
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-07 19:16:47 -0300

    Update fscloud-index.js

--- Server/Files/fscloud-index.js
+++ Server/Files/fscloud-index.js
@@ -1,7 +1,7 @@
 function truncateWithEllipses(text, max) {    return text.substr(0,max-1)+(text.length>max?'&hellip;':''); }    
 function truncgate(input) {   return truncateWithEllipses(input, 100);}  
 
-function si_thud(inputz) {       var zsi_thudsss="";//<img class=\"fa\" style='display:none;'/>";  
+function si_thud(inputz) {       var zsi_thudsss="";//<img class=\"fa\" style='display:none;'/>";     
   if(loacol_thum=="true"){ zsi_thudsss=inputz; }     return zsi_thudsss;   }    
 
 

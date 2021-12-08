9cc0d8159ceefc37a3a3ac66be5c4570b9394710
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-08 00:09:59 -0300

    Update fs-player.oncore.js

--- fs-player.oncore.js
+++ fs-player.oncore.js
@@ -603,8 +603,16 @@ function m_Mouse() {    pl_dispNav("plvideo").style.cursor = "auto";  pl_dispNav
 
 
 
-kvolldsisaiopl.addEventListener("mousemove", zzgdddeltttmecurrent);
-kddvarbararvideopl.addEventListener("mousemove", geltttmecurrent);  kddvarbararvideopl.addEventListener("mousedown", geltttmecurrent);kddvarbararvideopl.addEventListener("mouseup", geltttmecurrent);
+function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+}
+
+	kvolldsisaiopl.addEventListener("mousemove", zzgdddeltttmecurrent);
+kddvarbararvideopl.addEventListener("mousemove", geltttmecurrent);  
+kddvarbararvideopl.addEventListener("mousedown", function(){document.onmouseup = closeDragElement;
+    document.onmousemove = geltttmecurrent;});
+//kddvarbararvideopl.addEventListener("mouseup", geltttmecurrent);
 
 
 z_setid_even_lit(video, "abort", function(){}); z_setid_even_lit(video, "error", function(){}); z_setid_even_lit(video, "play", function(){updatePlayButton();  toggleds();fdpl_showl_pld(false);}); z_setid_even_lit(video, "pause", function(){updatePlayButton();  toggleds();}); z_setid_even_lit(video, "playing", function(){fdpl_showiddl_pld('loadedp',false);mdplprogressgetpl(video,"progress_soj");}); z_setid_even_lit(video, "waiting", function(){fdpl_showiddl_pld('loadedp',true);}); z_setid_even_lit(video, "ended", function(){ var loadd_rte=video.playbackRate;update_all();fdpl_showl_pld(true);video.load();  video.playbackRate=loadd_rte; });

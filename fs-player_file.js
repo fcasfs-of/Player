757496c80fcf0627ddce7634fbc6dc49531ac276
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-08 19:48:40 -0300

    Update fs-player_file.js

--- fs-player_file.js
+++ fs-player_file.js
@@ -1,6 +1,5 @@
 
 Reproduce_aLink(zsssasd_iswe,"zpreviewdemovideinz");
 
-//fs_addpost_fl("pl-freeoif").create("contx-io","plugins_fs","oi-info","titl-io","des-oi","tags-oi","list_od");
-
+fs_addpost_fl("pl-freeoif").create("contx-io","plugins_fs","oi-info","titl-io","des-oi","tags-oi","list_od");
 

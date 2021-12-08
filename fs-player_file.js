bbd3961cdd7c841fc17e4d85f301fa20342dadbf
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-08 19:53:10 -0300

    Update fs-player_file.js

--- fs-player_file.js
+++ fs-player_file.js
@@ -1,5 +1,13 @@
 
-Reproduce_aLink(zsssasd_iswe,"zpreviewdemovideinz");
+function fs_addpost_fl(idd){  var fs_lightbiddrame = document.getElementById(idd);   
+return { 
+create:function(tt,tug,rr,ti,de,tg,zl){fs_lightbiddrame.innerHTML='<div class="blog-container">  <div class="blog-header" style="display:none;">    <div class="blog-cover">      <div class="blog-author">        <h3></h3>      </div>    </div>  </div>  <div class="blog-body">  <div style="padding:4px;" id="'+tt+'"><div id="'+tug+'"></div></div>    <span id="'+rr+'">   <div class="blog-title">      <h1><a id="'+ti+'"></a></h1>    </div>        <div class="blog-summary">      <p id="'+de+'"></p>    </div>    <div class="blog-tags">  <ul id="'+tg+'">      </ul>    </div>    </span>  </div> <div class="blog-footer">    <ul id="'+zl+'">   </ul>  </div></div>'; },
+};
+}
 
 fs_addpost_fl("pl-freeoif").create("contx-io","plugins_fs","oi-info","titl-io","des-oi","tags-oi","list_od");    
+
+
+
+Reproduce_aLink(zsssasd_iswe,"zpreviewdemovideinz");
 

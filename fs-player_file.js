6a721e2408a5b71acab4069d0149c528a7026768
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-10 18:14:40 -0300

    Update fs-player_file.js

--- fs-player_file.js
+++ fs-player_file.js
@@ -1,5 +1,5 @@
 
-function fs_addpost_fl(idd){  var fs_lightbiddrame = document.getElementById(idd);   
+function fs_addpost_fl(idd){  var fs_lightbiddrame = document.getElementById(idd);     
 return { 
 create:function(tt,tug,rr,ti,de,tg,zl){fs_lightbiddrame.innerHTML='<div class="blog-container">  <div class="blog-header" style="display:none;">    <div class="blog-cover">      <div class="blog-author">        <h3></h3>      </div>    </div>  </div>  <div class="blog-body">  <div style="padding:4px;" id="'+tt+'"><div id="'+tug+'"></div></div>    <span id="'+rr+'">   <div class="blog-title">      <h1><a id="'+ti+'"></a></h1>    </div>        <div class="blog-summary">      <p id="'+de+'"></p>    </div>    <div class="blog-tags">  <ul id="'+tg+'">      </ul>    </div>    </span>  </div> <div class="blog-footer">    <ul id="'+zl+'">   </ul>  </div></div>'; },
 };

e43da127ab13331415b26ba26d57fba86f9257e9
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-13 15:55:58 -0300

    Update fscloud-index.js

--- Server/Files/fscloud-index.js
+++ Server/Files/fscloud-index.js
@@ -7,6 +7,9 @@ function truncgate(input) {   return truncateWithEllipses(input, 100);   }
 function si_thud(inputz) {       var zsi_thudsss="";//<img class=\"fa\" style='display:none;'/>";     
   if(loacol_thum=="true"){ zsi_thudsss=inputz; }     return zsi_thudsss;   }    
 
+function si_rrrthud(inputz, fh) {       var zsi_thudsss="";       if(inputz==true){ zsi_thudsss=inputz; }     return zsi_thudsss;   }    
+
+
 function si_thudicon(zzz, inputz, inddputz) {   var zsi_tddddfdfdhudsss="";    var zsi_tdfdfdhudsss="";    var zsid_fdtdfdfdhudsss="";  var zsi_thudsss="fl";     
     if(inputz=="fil"){  zsid_fdtdfdfdhudsss="data-tip='File: '";  }   if(inputz=="text"){ zsi_thudsss="tx";  zsid_fdtdfdfdhudsss="data-tip='Text: '";  }   if(inputz=="photo"){ zsi_thudsss="pic";  zsid_fdtdfdfdhudsss="data-tip='Photo: '";  }  if(inputz=="document"){ zsi_thudsss="doc";   zsid_fdtdfdfdhudsss="data-tip='Document: '";  }  if(inputz=="music"){ zsi_thudsss="musi";  zsid_fdtdfdfdhudsss="data-tip='Áudio: '";    if(inddputz==true){ zsi_tdfdfdhudsss=" bt"; }  }  if(inputz=="video"){ zsi_thudsss="vid";  zsid_fdtdfdfdhudsss="data-tip='Video: '";    if(inddputz==true){ zsi_tdfdfdhudsss=" bt"; }  }   if(inputz=="play"){ zsi_thudsss="vf"; } 
 if(zzz==true){  if(zsid_fdtdfdfdhudsss==""){  }  else  {zsi_tddddfdfdhudsss="tip";  }  }  
@@ -14,35 +17,35 @@ return { ty:{c:zsi_tddddfdfdhudsss,t:""+zsid_fdtdfdfdhudsss}, ic: ""+zsi_thudsss
 
 
 
-function add_fileset(zza, zz,nm,aaa,m,bb,nn) {   var adffffd_fileset="";   if(zza==true){  adffffd_fileset="display:none;";  }    var add_filesetsss=si_thudicon(aaa,zz,nm);      
-    return '<li class="'+add_filesetsss.ic+'"><a class="'+add_filesetsss.ty.c+'" style="cursor:pointer;'+adffffd_fileset+'" onclick="ssdo_vieimad_vietdd(\''+m+'\', \''+nn+'\',\'\')">'+si_thud("<img src=\""+bb+"\" class=\"fa\"/>")+'<span '+add_filesetsss.ty.t+'>'+truncgate(nn)+'</span></a></li>';    }       
+function add_fileset(fdgf, zza, zz,nm,aaa,m,bb,nn) {   var adffffd_fileset="";   if(zza==true){  adffffd_fileset="display:none;";  }    var add_filesetsss=si_thudicon(aaa,zz,nm);      
+    return '<li class="'+add_filesetsss.ic+'"><a class="'+si_rrrthud(fdgf,add_filesetsss.ty.c)+'" style="cursor:pointer;'+adffffd_fileset+'" onclick="ssdo_vieimad_vietdd(\''+m+'\', \''+nn+'\',\'\')">'+si_thud("<img src=\""+bb+"\" class=\"fa\"/>")+'<span '+add_filesetsss.ty.t+'>'+truncgate(nn)+'</span></a></li>';    }       
 
 
 var loacol9d="https://fcasfs-of.github.io/Player/Server/Files/cloud";
 
 
 
-function additebfnnd(dsdsd,targf_ddlist_filesaaaet,kl,kk){
+function additebfnnd(dsdsd,targf_ddlist_filesaaaet,kl,kk, fdgf){
 //var targf_ddlist_filesaaaet = '';
 
 
 
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"video",kl,kk,loacol9d+"/video/711600070003432847011811.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/[MV] Ryukendo_000354.jpg","[MV] Ryukendo");
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"video",kl,kk,loacol9d+"/video/714590675060111840304810.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Movie - All Songs_000508.jpg","Jiban the movie - All Songs");
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"video",kl,kk,loacol9d+"/video/411400070043002847020800.html","https://archive.org/download/tokusatsu-compilation-musics-all/tokusatsu-compilation-musics-all.thumbs/Tokusatsu - Compilation Musics_000164.jpg","Tokusatsu - Compilation Musics");
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"video",kl,kk,loacol9d+"/video/714590675060132807324890.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Kidou Keiji Jiban - Hit Song Collection_000027.jpg","Kidou Keiji Jiban - Hit Song Collection");
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"video",kl,kk,loacol9d+"/video/714590675060132805020894.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Kamen Rider BLACK RX - Full Songs_002036.jpg","Kamen Rider BLACK RX - Full Songs");
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"video",kl,kk,loacol9d+"/video/711678979903432040410011.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Space_Sheriff_Memorial - PV_000267.jpg","Space_Sheriff_Memorial - PV");
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"video",kl,kk,loacol9d+"/video/712690675063432847324893.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Sekai Ninja Sen Jiraiya - Full Songs_000294.jpg","Sekai Ninja Sen Jiraiya - Full Songs");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"video",kl,kk,loacol9d+"/video/711600070003432847011811.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/[MV] Ryukendo_000354.jpg","[MV] Ryukendo");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"video",kl,kk,loacol9d+"/video/714590675060111840304810.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Movie - All Songs_000508.jpg","Jiban the movie - All Songs");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"video",kl,kk,loacol9d+"/video/411400070043002847020800.html","https://archive.org/download/tokusatsu-compilation-musics-all/tokusatsu-compilation-musics-all.thumbs/Tokusatsu - Compilation Musics_000164.jpg","Tokusatsu - Compilation Musics");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"video",kl,kk,loacol9d+"/video/714590675060132807324890.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Kidou Keiji Jiban - Hit Song Collection_000027.jpg","Kidou Keiji Jiban - Hit Song Collection");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"video",kl,kk,loacol9d+"/video/714590675060132805020894.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Kamen Rider BLACK RX - Full Songs_002036.jpg","Kamen Rider BLACK RX - Full Songs");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"video",kl,kk,loacol9d+"/video/711678979903432040410011.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Space_Sheriff_Memorial - PV_000267.jpg","Space_Sheriff_Memorial - PV");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"video",kl,kk,loacol9d+"/video/712690675063432847324893.html","https://archive.org/download/tokusastsu-hit-song-collection/tokusastsu-hit-song-collection.thumbs/Sekai Ninja Sen Jiraiya - Full Songs_000294.jpg","Sekai Ninja Sen Jiraiya - Full Songs");
 
 
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"music",kl,kk,loacol9d+"/audio/711000175000102800114110.html","https://fcasfs-of.github.io/Player/Server/Files/Audio_Spectrum-648789.jpg","Space Squad: Gavan vs Dekaranger - music 02");
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"music",kl,kk,loacol9d+"/audio/711000175000002220124101.html","https://fcasfs-of.github.io/Player/Server/Files/Audio_Spectrum-648789.jpg","Space Squad: Gavan vs Dekaranger - music 01");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"music",kl,kk,loacol9d+"/audio/711000175000102800114110.html","https://fcasfs-of.github.io/Player/Server/Files/Audio_Spectrum-648789.jpg","Space Squad: Gavan vs Dekaranger - music 02");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"music",kl,kk,loacol9d+"/audio/711000175000002220124101.html","https://fcasfs-of.github.io/Player/Server/Files/Audio_Spectrum-648789.jpg","Space Squad: Gavan vs Dekaranger - music 01");
 
 
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"photo",kl,kk,loacol9d+"/photo/451450570543505847050855.html","https://archive.org/download/784757575712/1377976_230563587119799_822013184_n.jpg","Jiraiya vs Dokusai");
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"photo",kl,kk,loacol9d+"/photo/451451111141505847050855.html","https://archive.org/download/784757575712/IMG_118522831103.jpeg","Juspion vs MacGaren");
-targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(dsdsd,"photo",kl,kk,loacol9d+"/photo/421440570524407187054454.html","https://archive.org/download/784757575712/78 (9).jpg","Juspion: MacGaren 1");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"photo",kl,kk,loacol9d+"/photo/451450570543505847050855.html","https://archive.org/download/784757575712/1377976_230563587119799_822013184_n.jpg","Jiraiya vs Dokusai");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"photo",kl,kk,loacol9d+"/photo/451451111141505847050855.html","https://archive.org/download/784757575712/IMG_118522831103.jpeg","Juspion vs MacGaren");
+targf_ddlist_filesaaaet=targf_ddlist_filesaaaet+add_fileset(fdgf, dsdsd,"photo",kl,kk,loacol9d+"/photo/421440570524407187054454.html","https://archive.org/download/784757575712/78 (9).jpg","Juspion: MacGaren 1");
 
 return targf_ddlist_filesaaaet;   }
 

c135393fdbc3daa61f85ef2c9044e78b0deee180
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-17 15:25:57 -0300

    Update fs-player.onplugin.js

--- fs-player.onplugin.js
+++ fs-player.onplugin.js
@@ -35,7 +35,7 @@ function SelectfTdfext(element) {
 
 
 
-var wqqzasdfsfgsswwfltl = ""+'<div class="comments">    <h2>Leave a comment</h2>    <form class="comments__form" onsubmit="setcoome(event)">   <span class="comments__form-submit" style="position:absolute;top:1px;right:8px" onclick="mdpl_ztoogle_dispNav(wqqzasdffgdgsswwmdpssswwfltl);">X</span>    <div class="comments__form-info">        <div class="comments__form-field">          <input id="comments__form-label-name" name="author" placeholder="Name" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-name">            <span class="comments__form-label-text">Name</span>          </label>        </div>        <div class="comments__form-field">          <input id="comments__form-label-mail" name="author" placeholder="E-mail" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-mail">            <span class="comments__form-label-text">E-mail</span>          </label>        </div>        <div class="comments__form-field">          <input id="comments__form-label-site" name="author" placeholder="Website" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-site">            <span class="comments__form-label-text">Website</span>          </label>        </div>      </div>      <div class="comments__form-text">    <div class="comments__form-field">          <textarea id="comments__form-label-text" name="author" placeholder="Express your thoughts" type="text" class="comments__form-input comments__form-textarea"></textarea>          <label class="comments__form-label" for="comments__form-label-text">            <span class="comments__form-label-text">Express your thoughts</span>          </label>        </div>      </div>  <input name="submit" type="submit" id="submit"  disabled="disabled" class="button comments__form-submit" value="Post Comment">    </form>  <br/>   <div class="comments__list">      <!-- Comments -->     </div>  </div><br/>'+"";
+var wqqzasdfsfgsswwfltl = ""+'<div id="disqus_thread"></div>';//'<div class="comments">    <h2>Leave a comment</h2>    <form class="comments__form" onsubmit="setcoome(event)">   <span class="comments__form-submit" style="position:absolute;top:1px;right:8px" onclick="mdpl_ztoogle_dispNav(wqqzasdffgdgsswwmdpssswwfltl);">X</span>    <div class="comments__form-info">        <div class="comments__form-field">          <input id="comments__form-label-name" name="author" placeholder="Name" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-name">            <span class="comments__form-label-text">Name</span>          </label>        </div>        <div class="comments__form-field">          <input id="comments__form-label-mail" name="author" placeholder="E-mail" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-mail">            <span class="comments__form-label-text">E-mail</span>          </label>        </div>        <div class="comments__form-field">          <input id="comments__form-label-site" name="author" placeholder="Website" type="text" class="comments__form-input">          <label class="comments__form-label" for="comments__form-label-site">            <span class="comments__form-label-text">Website</span>          </label>        </div>      </div>      <div class="comments__form-text">    <div class="comments__form-field">          <textarea id="comments__form-label-text" name="author" placeholder="Express your thoughts" type="text" class="comments__form-input comments__form-textarea"></textarea>          <label class="comments__form-label" for="comments__form-label-text">            <span class="comments__form-label-text">Express your thoughts</span>          </label>        </div>      </div>  <input name="submit" type="submit" id="submit"  disabled="disabled" class="button comments__form-submit" value="Post Comment">    </form>  <br/>   <div class="comments__list">      <!-- Comments -->     </div>  </div><br/>'+"";
 
 
 var wqqzasdfsfgsswwfltlz = '<div><pre class="code wp-block-code code-html" onclick="vop_emjjg();" style="background:#fff;"><label style="text-align:center;">embed</label><code id="embe-ilss">&lt;iframe src="'+location.href+'" width="320" frameborder="0" height="240" style="height:100%;width:100%;" allowfullscreen=""&gt;&lt;/iframe&gt;</code><hr/><pre class="first wp-block-code"><span style="color:#fff;">?embed=true  -(preview of file)<br/>?info=true  -(view info file)</span></pre></pre></div>';
@@ -46,6 +46,18 @@ var wqqzasdfsswwmdpssswwfltl = document.getElementById('vtnsharre');     wqqzasd
 var wqqzasdffgdgsswwmdpssswwfltl = document.getElementById('lightcomme_box_modal');     wqqzasdffgdgsswwmdpssswwfltl.style.display="none"+"";
 
    var wqqzasdfdffsswwmdpssswwfltl = document.getElementById('lightcomme_box_modal');     wqqzasdfdffsswwmdpssswwfltl.innerHTML="<div style='padding:4px;'>"+wqqzasdfsfgsswwfltl+"</div><br/><br/><br/><br/><br/>";
+
+var disqus_config = function () {
+                this.page.url = location.href; 
+                this.page.identifier = ""; 
+            };
+    (function() { 
+    var d = document, s = d.createElement('script');
+    s.async = true;
+    s.src = 'https://fcasfs-of.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
 
 
 

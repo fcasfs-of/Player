c157955d1af3e4474db524d6de805eccdd5f4d30
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-28 18:33:11 -0300

    Update users.js

--- Accounts/users.js
+++ Accounts/users.js
@@ -1,7 +1,6 @@
 function id_proff(di){   var id_proffsss={ name:"", img:"", desc:"", info:"" };
- if (di=="85843503534"){ id_proffsss={ name:"Admin", img:"https://fcasfs-of.github.io/Player/fav_icon.png", desc:"Admin Profile", info:"User Basic" }; }
+ if (di=="85843503534"){   var id_proffsss={ name:"Admin", img:"https://fcasfs-of.github.io/Player/fav_icon.png", desc:"Admin Profile", info:"User Basic" }; }
                    
 return id_proffsss; }
 
- 
     

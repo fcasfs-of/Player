372c61a45d031379547ab26fbfea67f6726d4895
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-27 23:08:50 -0300

    Update fs-player.oncore.js

--- fs-player.oncore.js
+++ fs-player.oncore.js
@@ -23,11 +23,11 @@ var fcasmdplex_loadex_premuin_fl = function (zs,dss) {  return { set:function(d)
 },setcomment:function(ds,ss){
 	ds.innerHTML="<div style='padding:4px;'>"+fcasmdplex_loadex_premuin_fl("this").set_comment(ss)+"</div><br/><br/><br/><br/><br/>"
 },
-	pro:function(z,nn){      	   if(zs==true){   fs_embed_filepreminit(location.href).load();   cmmpri=true;
+	pro:function(z,nn){      	   if(zs==true){    cmmpri=true;
 	var zzszhirtertcfeardsscz=document.getElementById(dss);   
 zzszhirtertcfeardsscz.innerHTML=""+fcasmdplex_loadex_premuin_fl("this").set(z);    
   window.onload=function(){   var wqqzasdfddffsswwmdpssswwfltl = document.getElementById('lightcomme_box_modal');    fcasmdplex_loadex_premuin_fl("this").setcomment(wqqzasdfddffsswwmdpssswwfltl, "The Comments for this File not available");  };
-  window.onload=function(){  var fs_is_preemfil=fs_embed_filepreminit(location.href).is();  }
+  fs_embed_filepreminit(location.href).load();  window.onload=function(){  var fs_is_preemfil=fs_embed_filepreminit(location.href).is();  }
 }   else { nn(); }  } };    };  
     
 

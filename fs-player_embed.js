144200d0af45d9bb16ed8d725013ea68b572fac3
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-27 23:31:29 -0300

    Update fs-player_embed.js

--- fs-player_embed.js
+++ fs-player_embed.js
@@ -8,6 +8,6 @@ var zssdsffdggfsasd_iswe = ""+fcasmdplex_loadex.open("?info=")+"";
 var zssfdggfsasd_iswe = ""+fcasmdplex_loadex.open("?embed=")+"";
   var fs_embed_callb_io= fs_embed_init(location.href).is();    var fs_embed_callb_io_fl=fs_embed_callb_io.file;
 if (zssdsffdggfsasd_iswe=="true"){   fs_embed_callb_io_fl.info();   }  if (zssfdggfsasd_iswe=="true"){  fs_embed_callb_io_fl.embed();  }  else{  fs_embed_callb_io_fl.post();  }
-  window.onload=function(){  var fs_is_pfil=fs_embed_init(location.href).is();  }
+  window.addEventListener("load", function(){  var fs_is_pfil=fs_embed_init(location.href).is();  });
 }
 

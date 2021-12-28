1b4c64cb673fadd7326443706cbec446b906a114
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-28 17:43:14 -0300

    Create 85843503534.js

---
+++ Account/85843503534.js
@@ -0,0 +1,11 @@
+function aspor(){
+  var dsfdsftitle="Admin";
+  var dsfdsfimf="https://fcasfs-of.github.io/Player/fav_icon.png";
+  var dsfdsftsubtte="User Basic";
+  var dsfdsftinfe="Admin Profile";
+ 
+  document.title="FS Player: Account: "+dsfdsftitle;
+return '<div class="card Profile"><img src="'+dsfdsfimf+'" onclick="do_vieimad_vietdd(\'image\',\''+dsfdsfimf+'\', \''+dsfdsftitle+'\')" style="cursor:pointer;width:100%"><h1>'+dsfdsftitle+'</h1><p class="title">'+dsfdsftsubtte+'</p><p>'+dsfdsftinfe+'</p>  <div style="margin: 24px 0; display:none;">  </div>  <p style="display:none;"><button></button></p></div>';
+}
+    
+    

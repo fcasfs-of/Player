8397309fadd2e56f17dbb7cdf723fe8beb4177c0
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2021-12-27 22:44:51 -0300

    Update fs-player_embed.js

--- fs-player_embed.js
+++ fs-player_embed.js
@@ -1,7 +1,13 @@
+var fs_embed_list = {};
+function fs_embed_init(link){ return { is:function(){ return fs_embed_list[link]; }, load:function(p,i,e){  var fs_embed_list[link] = { file: { post: p, info: i, embed: e } }; }  }; }
+
+
 function fs_embed_callb(vd, fd, mg){
+  fs_embed_init(location.href).load(fd,mg,vd);
 var zssdsffdggfsasd_iswe = ""+fcasmdplex_loadex.open("?info=")+"";
 var zssfdggfsasd_iswe = ""+fcasmdplex_loadex.open("?embed=")+"";
-if (zssdsffdggfsasd_iswe=="true"){   mg();   }  if (zssfdggfsasd_iswe=="true"){  vd();  }  else{  fd();  }
+  var fs_embed_callb_io= fs_embed_init(location.href).is();    var fs_embed_callb_io_fl=fs_embed_callb_io.file;
+if (zssdsffdggfsasd_iswe=="true"){   fs_embed_callb_io_fl.info();   }  if (zssfdggfsasd_iswe=="true"){  fs_embed_callb_io_fl.embed();  }  else{  fs_embed_callb_io_fl.post();  }
+  window.onload=function(){  fs_embed_init(location.href).load(fd,mg,vd);  }
 }
-
 

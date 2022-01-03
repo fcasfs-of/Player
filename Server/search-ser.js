a4fc08bbe29b3ef59e73eee5baf201d3108ace57
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2022-01-03 17:59:24 -0300

    Update search-ser.js

--- Server/search-ser.js
+++ Server/search-ser.js
@@ -5,3 +5,17 @@ var targf_list_fidsflesaaaet = document.getElementById("q");  targf_list_fidsfle
 var tadrgf_list_fidsflesaaaet = document.getElementById("f_mfdval_Input");   document.title="FS Player - Online: Search: "+targf_list_fidsflesaaaet.value;
 //tadrgf_list_fidsflesaaaet.onsubmit=function(){location.href="https://fcasfs-of.github.io/Player/Server/search.htm?q="+targf_list_fidsflesaaaet.value;};
 
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+filterSelection("all");
+
+

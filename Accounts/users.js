cbc52e616ae47b037035e0d5941c6d143e6d3d28
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2022-01-07 20:35:16 -0300

    Update users.js

--- Accounts/users.js
+++ Accounts/users.js
@@ -1,5 +1,8 @@
+var thumbuseppcc="https://fcasfs-of.github.io/Player/Accounts/thumb/";
+
 function id_proff(di){   var id_proffsss={ note:"", alert:"", val:false, name:"", img:"", desc:"", info:"", list:"", btn:"" };
  if (di=="85843503534"){   var id_proffsss={ val:true, alert:"Account Demo", note:"Profile Official", name:"Admin", img:"https://fcasfs-of.github.io/Player/fav_icon.png", desc:"Admin Profile", info:"User Basic", list:"", btn:"" }; }
+ if (di=="10091312235554"){   var id_proffsss={ val:true, alert:"", note:"Profile Official", name:"Ana Paula de Sales", img:thumbuseppcc+"FaceApp_1631993051325 - Ana Paula S.jpg", desc:"", info:"", list:"", btn:"" }; }
                    
 return id_proffsss; }
 
